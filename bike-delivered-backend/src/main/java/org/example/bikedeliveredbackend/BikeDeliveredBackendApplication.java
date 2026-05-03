e60575fdf05506ce4fde6dc86ce87148e48e6419
Sourabh_g_ <saurabhbante11@gmail.com>
2026-05-03 22:43:03 +0530

    Backend Created 03-05-2026

---
+++ bike-delivered-backend/src/main/java/org/example/bikedeliveredbackend/BikeDeliveredBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.bikedeliveredbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeDeliveredBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BikeDeliveredBackendApplication.class, args);
+    }
+
+}
